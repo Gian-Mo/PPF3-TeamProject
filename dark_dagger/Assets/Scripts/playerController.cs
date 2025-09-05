7ee5ef0b8a5867bfab263dd80b137f77363733f7
Gian-Mo <gmr.11.sc@gmail.com>
2025-09-05 14:09:44 -0400

    Fixed PlayerController script

--- dark_dagger/Assets/Scripts/playerController.cs
+++ dark_dagger/Assets/Scripts/playerController.cs
@@ -7,9 +7,10 @@ public class playerController : MonoBehaviour
     [SerializeField] int HP;
     [SerializeField] int speed;
     [SerializeField] GameObject model;
+    [SerializeField] Animator anim;
+    [SerializeField] int animTranSpeed;
 
-
-   public Vector3 playerVel;
+    public Vector3 playerVel;
 
     public InputActionReference move;
     public InputActionReference shoot;
@@ -30,21 +31,32 @@ public class playerController : MonoBehaviour
     void Move()
     {
 
-            playerVel = move.action.ReadValue<Vector3>();
-            playerVel *= speed * Time.deltaTime;
+        playerVel = move.action.ReadValue<Vector3>();
+        playerVel = playerVel.normalized * speed * Time.deltaTime;
         if (move.action.IsPressed())
         {
             model.transform.rotation = Quaternion.Lerp(model.transform.rotation,Quaternion.LookRotation(playerVel.normalized),10 * Time.deltaTime); 
         }
 
        controller.Move(playerVel);
-        
-        
+
+        SetAnimLoco();
+
 
     }
+    void SetAnimLoco()
+    {
+        float agentSpeedCur = playerVel.normalized.magnitude;
+        float animSpeedCur = anim.GetFloat("Speed");
 
+        anim.SetFloat("Speed", agentSpeedCur);
+
+    }
     void Shoot()
     {
+        //Ray cast from the player head (change to hand later) towards the mouse position, distance is set by the weapon scriptable object(later)
+
+
 
     }
 }
