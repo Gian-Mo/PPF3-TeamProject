6f2cf7ed2e22e05bfad235cd94b41e830c2609d1
Rodoc0222 <rodoc0222@gmail.com>
2025-09-10 10:49:03 -0400

    model now textured, enemy now moves shoots and dies, no anims

---
+++ dark_dagger/Assets/Scripts/enemyAI_basic.cs
@@ -0,0 +1,172 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class enemyAI_basic : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform headPos;
+    [SerializeField] Animator anim;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int FOV;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamPauseTime;
+    [SerializeField] int animTransSpeed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    [SerializeField] Transform shootPos;
+
+    Color colorOrig;
+
+    float shootTimer;
+    float roamTimer;
+    float angletoPlayer;
+    float stoppingDistOrig;
+
+    bool PlayerinTrigger;
+
+    Vector3 playerDir;
+    Vector3 startingPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+        startingPos = transform.position;
+        stoppingDistOrig = agent.stoppingDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setAnimLoco();
+
+        shootTimer += Time.deltaTime;
+
+        if (agent.remainingDistance <= 0.01f)
+        {
+            roamTimer += Time.deltaTime;
+        }
+
+        if (PlayerinTrigger && !canSeePlayer())
+        {
+            checkRoam();
+        }
+        else if (!PlayerinTrigger)
+        {
+            checkRoam();
+        }
+    }
+
+    void setAnimLoco()
+    {
+        float agentSpeedCur = agent.velocity.normalized.magnitude;
+        float animSpeedCur = anim.GetFloat("Speed");
+
+        anim.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeedCur, Time.deltaTime * animTransSpeed));
+    }
+
+    void checkRoam()
+    {
+        if (roamTimer >= roamPauseTime && agent.remainingDistance < 0.01f)
+        {
+            roam();
+        }
+    }
+
+    void roam()
+    {
+        roamTimer = 0;
+
+        agent.stoppingDistance = 0;
+
+        Vector3 randPos = Random.insideUnitSphere * roamDist;
+        randPos += startingPos;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randPos, out hit, roamDist, 1);
+        agent.SetDestination(hit.position);
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = GameManager.instance.player.transform.position - headPos.position;
+        angletoPlayer = Vector3.Angle(playerDir, transform.forward);
+        Debug.DrawRay(headPos.position, playerDir);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            //if I can see you
+            if (hit.collider.CompareTag("Player") && angletoPlayer <= FOV)
+            {
+                agent.SetDestination(GameManager.instance.player.transform.position);
+
+                if (shootTimer >= shootRate)
+                {
+                    shoot();
+                }
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                agent.stoppingDistance = stoppingDistOrig;
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerinTrigger = true;
+            agent.stoppingDistance = 0;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        anim.SetTrigger("Shoot");
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+
+    public void takeDamage(int amount)
+    {
+
+        if (HP > 0)
+        {
+            HP -= amount;
+            StartCoroutine(flashRed());
+
+            agent.SetDestination(GameManager.instance.player.transform.position);
+        }
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+}
