0f7849931e1f7711480fb31cd165df52ac0c3df1
Carlos Rodo Zhang <rodoc0222@gmail.com>
2025-09-28 00:01:44 -0400

    code added for quit to work on webgl, player settings changed to enforce resolution, again, it was likely overridden when switching to webgl build.

--- dark_dagger/Assets/Scripts/MainMenu.cs
+++ dark_dagger/Assets/Scripts/MainMenu.cs
@@ -57,16 +57,16 @@ public class MainMenuManager : MonoBehaviour
 
     public void Quit()
     {
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 
+    #elif UNITY_WEBGL
+        Application.OpenURL("https://rodoc0222.itch.io/dark-dagger");
+    #else
+        Application.Quit();
 
-#else
 
-         Application.Quit(); Application.Quit();
-
-
-#endif
+    #endif
 
     }
 
